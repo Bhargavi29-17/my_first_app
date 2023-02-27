b7b5255dc9d576106aee1bc9d62ea0b963beeb01
Bhargavi29-17 <122281432+Bhargavi29-17@users.noreply.github.com>
2023-02-27 09:48:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Hey :blue[Amigos]: :sunglasses:")
+st.subheader("This Message is for You")
+btn_click=st.button("click Here!")
+st.snow()
+if btn_click == True:
+    st.header("Save Nature :deciduous_tree: Save Earth :earth_asia:")
